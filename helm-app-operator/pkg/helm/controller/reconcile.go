823cdeb5cbc4effdb53ecab8c61bfc7aa98909eb
Joe Lanford <joe.lanford@gmail.com>
2018-10-11 11:26:31 -0400

    pkg/helm/controller: use finalizer for uninstalling helm release

--- helm-app-operator/pkg/helm/controller/reconcile.go
+++ helm-app-operator/pkg/helm/controller/reconcile.go
@@ -28,18 +28,51 @@ type helmOperatorReconciler struct {
 	mutex                sync.RWMutex
 }
 
+const (
+	finalizer = "uninstall-helm-release"
+)
+
 func (r *helmOperatorReconciler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	logrus.Infof("processing %s", request.NamespacedName)
 
 	o := &unstructured.Unstructured{}
 	o.SetGroupVersionKind(r.GVK)
 	err := r.Client.Get(context.TODO(), request.NamespacedName, o)
-	o.SetName(request.Name)
-	o.SetNamespace(request.Namespace)
+	if apierrors.IsNotFound(err) {
+		return reconcile.Result{}, nil
+	}
 	if err != nil {
-		if apierrors.IsNotFound(err) {
-			_, err = r.Installer.UninstallRelease(o)
+		return reconcile.Result{}, err
+	}
+
+	deleted := o.GetDeletionTimestamp() != nil
+	pendingFinalizers := o.GetFinalizers()
+	if !deleted && !contains(pendingFinalizers, finalizer) {
+		logrus.Debugf("adding finalizer %s to resource", finalizer)
+		finalizers := append(pendingFinalizers, finalizer)
+		o.SetFinalizers(finalizers)
+		err := r.Client.Update(context.TODO(), o)
+		return reconcile.Result{}, err
+	}
+	if deleted {
+		if !contains(pendingFinalizers, finalizer) {
+			logrus.Info("resouce is terminated, skipping reconciliation")
+			return reconcile.Result{}, nil
 		}
+
+		_, err = r.Installer.UninstallRelease(o)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+
+		finalizers := []string{}
+		for _, pendingFinalizer := range pendingFinalizers {
+			if pendingFinalizer != finalizer {
+				finalizers = append(finalizers, pendingFinalizer)
+			}
+		}
+		o.SetFinalizers(finalizers)
+		err := r.Client.Update(context.TODO(), o)
 		return reconcile.Result{}, err
 	}
 
@@ -63,6 +96,15 @@ func (r *helmOperatorReconciler) Reconcile(request reconcile.Request) (reconcile
 	r.setLastResourceVersion(request.NamespacedName, o.GetResourceVersion())
 
 	return reconcile.Result{RequeueAfter: r.ResyncPeriod}, nil
+}
+
+func contains(l []string, s string) bool {
+	for _, elem := range l {
+		if elem == s {
+			return true
+		}
+	}
+	return false
 }
 
 func (r *helmOperatorReconciler) getLastResourceVersion(n types.NamespacedName) (string, bool) {
